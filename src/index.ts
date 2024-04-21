2293d99f38cfa4de385a98421092c2fd0936d3a8
Linux Hari <vadiveluversionz1.0@gmail.com>
2024-04-21 15:49:48 +0530

    "origin set"

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,9 @@ if (environment === 'production') {
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL
+}));
 
 app.use('/api/my/user', myUserRoutes);
 
