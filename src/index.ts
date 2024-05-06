017081412e4bd7f9359693e234253b3ff8f60a30
Linux Hari <vadiveluversionz1.0@gmail.com>
2024-05-06 22:07:42 +0530

    "minor change"

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ cloudinary.config({
 
 const app = express();
 
-app.use(cors({origin: process.env.FRONTEND_URL}));
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 
 app.use('/api/order/checkout/webhook', express.raw({ type: '*/*' }));
 
